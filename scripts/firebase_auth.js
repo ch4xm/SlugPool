543d4daadbbf8e9023630be3d16dc3b37c406e44
Daksh Shah <dakshah@ucsc.edu>
2024-01-21 01:11:22 -0800

    add basic error check for auth

--- scripts/firebase_auth.js
+++ scripts/firebase_auth.js
@@ -5,14 +5,25 @@ import {
     onAuthStateChanged
 } from "firebase/auth";
 import {auth} from "./firebase_initialize";
+import { resolveUri } from "expo-asset/build/AssetSources";
 
 const AuthContext = createContext();
+export const AuthContextProvider = ( { children } ) => {
+    const GoogleSignIn = () => {
+        const provider = GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
 
-const GoogleSignIn = () => {
-    const provider = GoogleAuthProvider();
-    signInWithPopup(auth, provider)
+                
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.customData.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+            });
         
-      
-  };
+    };
 
 export default GoogleSignIn;
