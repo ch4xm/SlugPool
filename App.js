276c91d5af0e3a12274090d69e0c61e2dc1f27dd
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2024-01-20 18:46:49 -0800

    surpringyl few mergr conlirencts

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ import * as Location from 'expo-location';
 import React, { useState, useEffect } from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
 import { useRoute } from '@react-navigation/native';
 
